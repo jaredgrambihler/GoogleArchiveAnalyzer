fd378beeacc55941830ed1b2e38bcc84aff9bda0
JaredGrambihlerHome <jaredgrambihler@gmail.com>
2020-10-15 17:45:10 -0700

    Fixed photos code to use Path objects

--- GoogleArchive/photos.py
+++ GoogleArchive/photos.py
@@ -1,42 +1,41 @@
-"""
-Writes all URLs for Google Photos to a file
-"""
+"""Writes all URLs for Google Photos to a file."""
 import os
+import pathlib
 import json
 
-def photoURL(path, dir):
-    """
-    Creates an output file, Photo_URLs.txt based on the specified path.
+def photoURL(takeoutPath: str, outputFolder: str) -> None:
+    """Output data on photo URLS, if present.
+
+    Creates an output file, Photo_URLs.txt.
     File contains URLs of all photos on Google Photos.
     Prints out failure to find folder and number of file and folder errors encounters.
-    Args:
-        path: String, Current working directory
-        dir: String, subdirectory to store file at.
-    """
-    #Will ignore any folders/files that have an error. Prints out the
-    #number of errors upon termination.
-    folderErrors = 0
-    fileErrors = 0
-    f = open(path + dir + 'Photo_URLs.txt', 'w')
-    try:
-        for folder in os.listdir('../Takeout/Google Photos'):
-            try: 
-                for file in os.listdir('..\Takeout\Google Photos\\' + folder):
-                    try:
-                        if 'json' in file:
-                            photoFile = open('..\Takeout\Google Photos\\' + folder + '\\' + file)
-                            fileDict = json.loads(photoFile.read())
-                            f.write(fileDict["url"] + '\n\n')
-                    except:
-                        fileErrors += 1
-            except:
-                folderErrors += 1
-    except:
-        print('Google photo folder not found')
-    finally:
-        f.close()
 
-    if(fileErrors != 0):
-        print("Google Photos File Errors: " + str(fileErrors))
-    if(folderErrors != 0):
-        print("Google Photos Folder Errors: " + str(folderErrors))
+    Args:
+        takeoutPath (str): relative path to takeout folder
+        outputFolder (str): the name of the folder to output the data to in the
+            current working directory.
+    """
+    # Will ignore any folders/files that have an error. Prints out the
+    # number of errors upon termination.
+    takeoutPath = pathlib.Path(takeoutPath)
+    urlErrors = 0
+    photosFolder = pathlib.Path(os.getcwd()).joinpath(takeoutPath).joinpath("Google Photos")
+    if not photosFolder.exists():
+        print("Photos folder does not exist. Skipping photos output.")
+    if not photosFolder.is_dir():
+        print("Photos folder is not a directory. Skipping photos output.")
+    outputPath = pathlib.Path(os.getcwd()).joinpath(outputFolder.replace("\\", "")).joinpath('Photo_URLs.txt')
+    with outputPath.open('w') as f:
+        for folder in photosFolder.glob("*"):
+            for photoFile in folder.glob("*.json"):
+                if photoFile.name == "metadata.json":
+                    continue
+                with photoFile.open('r') as photoF:
+                    fileDict = json.loads(photoF.read())
+                    try:
+                        f.write(fileDict["url"] + '\n\n')
+                    except KeyError:
+                        urlErrors += 1
+
+    if(urlErrors > 0):
+        print("Google Photos URLs not found: {}".format(urlErrors))
